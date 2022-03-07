ae86f6ef151ab4692b15d55a3da6a717434ad7b9
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-03-07 14:25:31 +0530

    preclass 2 done

--- day_2_react_hook_counter/src/App.js
+++ day_2_react_hook_counter/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Counter from './components/Counter';
+import { Task } from './components/Task';
 
 function App() {
   return (
     <div className="App">
      
       <Counter/>
+      <Task/>
     </div>
   );
 }
