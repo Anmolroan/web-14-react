e4416c4c5363e65084f3e8126e08fa244b90841e
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-04-06 23:38:59 +0530

    done

--- api_context/src/component/Navbar.jsx
+++ api_context/src/component/Navbar.jsx
@@ -3,10 +3,11 @@ import Cart from './Cart'
 import {useContext} from 'react';
 import {ThemeContext} from "../contexts/ThemeContext"
 function Navbar() {
-    const {theme} =useContext(ThemeContext)
+    const {theme,toggleTheme} =useContext(ThemeContext)
   return (
     <div>
     <div>Navbar theme is {theme}</div>
+    <button onClick={toggleTheme}>Change theme</button>
         <Cart/>
     </div>
   )
