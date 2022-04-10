00817e3f6ea6a95216b20341e74f681bbd38da7d
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-04-10 21:11:24 +0530

    pagination done

--- useEffect/src/components/Todo.jsx
+++ useEffect/src/components/Todo.jsx
@@ -1,17 +1,19 @@
 import {useState ,useEffect} from "react"
 export const Todo =()=>{
-    useEffect(()=>{
-getTodo();
-    },[]);
+
     
     const [todos,setTodos] =useState([])
     const [text,setText] = useState("");
+    const [page,setPage] = useState(1)
     const getTodo = ()=>{
-        fetch("http://localhost:8000/todos").then((d)=>d.json()).then(res=>{
+        fetch(`http://localhost:8000/todos?_page=${page}&_limit=3`).then((d)=>d.json()).then(res=>{
              setTodos(res)
             console.log(res)
         })
     }
+    useEffect(()=>{
+        getTodo();
+            },[page]);
     return <div>
         <input value = {text} type ="text" placeholder ='enter todo' onChange={(e)=>setText(e.target.value) }/>
         <button onClick={()=>{
@@ -36,5 +38,7 @@ getTodo();
         )
             
         )}
+        <button onClick={()=>setPage(page-1)}>Prev</button>
+        <button onClick={()=>setPage(page+1)}>Next</button>
     </div>
 }
