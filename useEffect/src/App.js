169ae71a62046e92edf9ae4fe0378c0b55a3b679
Anmolroan <90396759+Anmolroan@users.noreply.github.com>
2022-04-10 23:21:43 +0530

    timer is done

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
-import { Todo } from "./components/Todo";
+import Timer from "./components/Timer";
+// import { Todo } from "./components/Todo";
 
 function App() {
+  const [show,setShow] =useState(true);
   return <div className="App">useEffect
   
-  <Todo/>
+  {/* <Todo/> */}
+  {show?<Timer/>:null}
+  <button onClick={()=>setShow(!show)}>{show?"Hide":"Show"}</button>
   </div>;
 }
 
---
+++ useEffect/src/components/Timer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect } from 'react'
+export default function Timer() {
+    const [counter,setCounter] = React.useState(5);
+    useEffect(()=>{
+const interval=setInterval(()=>{
+   
+    setCounter((p)=>{
+        if(p>0){
+            return p-1;
+        }else{
+            clearInterval(interval);
+            return p
+        }
+    })
+},1000)
+return ()=>{
+    clearInterval(interval)
+}
+    },[])
+  
+    return (
+        <div>
+        count is :{counter}
+        </div>
+      )
+  
+ 
+}
